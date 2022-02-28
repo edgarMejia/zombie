834b5d7313f4ce8ca1f431c0a3bf6032a06e7c96
Edgar Mejía <edgar13155@gmail.com>
2022-02-28 01:11:59 -0600

    Fix some typos.

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -12,7 +12,7 @@ export class AboutComponent implements OnInit {
     {
       name: 'Ivette Lara',
       username: '@ivette',
-      description: 'Yo soy la Lider de Gestíón de Marketing, me encarga de crear las estraregias de ventas de nuestros productos y llevarla a la mayor cantidad de lugares posibles donde los Zombies puedan atacar, en mis tiempos libres me gusta disfrutar de la playa y sus bellos atardeceres.',
+      description: 'Yo soy la Lider de Gestíón de Marketing, me encargo de crear las estraregias de ventas de nuestros productos y llevarlas a la mayor cantidad de lugares posibles donde los Zombies puedan atacar, en mis tiempos libres me gusta disfrutar de la playa y sus bellos atardeceres.',
       hashtags: ['#beachlover', '#sunset', '#ContraLosZombies'],
       coverPath: 'assets/images/about/ivette/cover.png',
       avatarPath: 'assets/images/about/ivette/avatar.png'
@@ -36,7 +36,7 @@ export class AboutComponent implements OnInit {
     {
       name: 'Alexis Gonzalez',
       username: '@Alexis',
-      description: 'Brains!!!!!!!, hola zombies, mi nombre es Alexis, y me encargo del apoyo digital dentro de la compañia para la generacion de productos contra Zombies, Zombies Preparence!!!!!!!',
+      description: 'Brains!!!!!!!, hola zombies, mi nombre es Alexis, y me encargo del apoyo digital dentro de la compañía para la generación de productos contra Zombies, Zombies Preparence!!!!!!!',
       hashtags: ['#Moungtain', '#ContraLosZombies', '#Señor', '#Climber'],
       coverPath: 'assets/images/about/alexis/cover.png',
       avatarPath: 'assets/images/about/alexis/avatar.png'
@@ -44,7 +44,7 @@ export class AboutComponent implements OnInit {
     {
       name: 'Edgar Mejía',
       username: '@edgarMmejia',
-      description: 'Hola mundo! Soy Edgar, actualmente me encargo del desarrollo de la tienda en linea de Zombie Repelent, me gustan los videojuegos, los gatos y muchas cosas más que ya no recuerdo.',
+      description: 'Hola mundo! Soy Edgar, actualmente me encargo del desarrollo de la tienda en linea de Zombie Repelent, me gustan los videojuegos, los gatos y muchas cosas más que ya no recuerdo. GG salu2.',
       hashtags: ['#Dev', '#Music', '#Cats'],
       coverPath: 'assets/images/about/edgar/cover.jpg',
       avatarPath: 'assets/images/about/edgar/avatar.jpg'
